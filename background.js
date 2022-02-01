9618fc534cb6d260232b0351c2d22c473f8b1b5d
phillip <phillip.iwanaga@gmail.com>
2022-01-31 18:35:20 -0800

    initial commit

---
+++ background.js
@@ -0,0 +1,230 @@
+// Listen for messages received from frontend (popup.js). If message received, check message text and perform action.
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "get_all") {
+        // Get all records from database
+        let get_all_request = get_all_records()
+
+        /*
+         * Respond with message and payload -- an array of objects representing our database records.
+         * Example payload:
+         * [{
+         *      title: "LinkedIn",
+         *      copyText: "www.linkedin.com/profile"
+         *  },
+         *  {
+         *      title: "Portfolio",
+         *      copyText: "www.portfolio.com"
+         *  }] 
+         */ 
+        get_all_request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "get_all_success",
+                payload: res
+            })
+        })
+    } else if (request.message === "insert") {
+        // Insert record from payload into database 
+        let insert_request = insert_record(request.payload)
+
+        /*
+         * Return message and an object representing the record that was inserted.
+         * Example payload:
+         * {
+         *  title: "Github",
+         *  copyText: "www.github.com/profile"     
+         * }
+         */
+        insert_request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "insert_success",
+                payload: res
+            })
+        })
+    } else if (request.message === "delete") {
+        // Delete record from database using the title passed in request.payload. Title is the keyPath used to look up records in the database.
+        let delete_request = delete_record(request.payload)
+        
+        // Delete record and return true or false depending on if deletion was successful
+        delete_request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "delete_success",
+                payload: res
+            })
+        })
+    }
+})
+
+let db = null
+
+const create_database = () => {
+    // Use self instead of window because background.js is a service worker and does not have access to DOM
+    const request = self.indexedDB.open("ClipboardDB")
+
+    request.onerror = event => {
+        console.log("Error opening DB.")
+    }
+    
+    // Gets executed whenever a new database in opened or if a different version of an existing database is opened. Does not get executed otherwise. This is also where the schema is defined.
+    request.onupgradeneeded = (event) => {
+        db = event.target.result
+
+        // Defines schema and keyPath, the value used to look up entries in DB.
+        let objectStore = db.createObjectStore("links", {
+            keyPath: "title"
+        })
+
+        objectStore.transaction.oncomplete = (event) => {
+            console.log("ObjectStore Created.")
+        }
+    }
+
+    // Always runs unless there is an error when opening database
+    request.onsuccess = (event) => {
+        db = event.target.result
+        console.log("DB Opened.")
+    }
+}
+
+const delete_database = () => {
+    const request = window.indexedDB.delete_database("ClipboardDB")
+
+    request.onerror = (event) => {
+        console.log("Error deleting DB.")
+    }
+
+    request.onsuccess = (event) => {
+        db = event.target.result
+
+        console.log("DB Deleted.")
+    }
+}
+
+// Retrieve all records from database
+const get_all_records = () => {
+    if (db) {
+        // Specify what table(s) this transaction will be affiliated with. In this case there is only one table. If there were more, an array of different table names could be used instead of just "links".
+        const get_transaction = db.transaction("links", "readonly")
+        
+        // Specify which table to readwrite on
+        const objectStore = get_transaction.objectStore("links")
+
+        // Because database actions are asynchronous, a Promise is created  and if resolved, returns all the records from the database
+        return new Promise((resolve, reject) => {
+            get_transaction.oncomplete = () => {
+                console.log("Get Transactions Complete.")
+            }
+    
+            get_transaction.onerror = () => {
+                console.log("Error getting records.")
+            }
+    
+            let request = objectStore.getAll()
+    
+            request.onsuccess = (event) => {
+                resolve(request.result)
+            }
+        })
+    }
+}
+
+// Add new record to database
+const insert_record = (record) => {
+    if (db) {
+        const insert_transaction = db.transaction("links", "readwrite")
+
+        const objectStore = insert_transaction.objectStore("links")
+
+        return new Promise((resolve, reject) => {
+            insert_transaction.oncomplete = () => {
+                console.log("Add Transactions Complete.")
+            }
+    
+            insert_transaction.onerror = () => {
+                console.log("Error adding records.")
+            }
+            
+            let request = objectStore.add(record)
+            // After adding record, retrieve the record and return it
+            let newRecord = get_record(record.title)
+            request.onsuccess = () => {
+                console.log("Added: ", record)
+                resolve(newRecord)
+            }
+        })
+    }
+}
+
+// Get single record from database by title
+const get_record = (title) => {
+    if (db) {
+        const get_transaction = db.transaction("links", "readonly")
+        
+        const objectStore = get_transaction.objectStore("links")
+
+        return new Promise((resolve, reject) => {
+            get_transaction.oncomplete = () => {
+                console.log("Get Transaction Complete.")
+            }
+    
+            get_transaction.onerror = () => {
+                console.log("Error getting record.")
+            }
+    
+            let request = objectStore.get(title)
+            
+            // Return record
+            request.onsuccess = (event) => {
+                resolve(event.target.result)
+            }
+        })
+    }
+}
+
+// Update a record. For now, this is not being used.
+// const update_record = (record) => {
+//     if (db) {
+//         const put_transaction = db.transaction("links", "readwrite")
+
+//         const objectStore = put_transaction.objectStore("links")
+
+//         return new Promise((resolve, rejct) => {
+//             put_transaction.oncomplete = () => {
+//                 console.log("Put Transactions Complete.")
+//                 resolve(true)
+//             }
+    
+//             put_transaction.onerror = () => {
+//                 console.log("Error updating records.")
+//                 resolve(false)
+//             }
+    
+//             objectStore.put(record)
+//         })
+//     }
+// }
+
+// Delete record from database
+const delete_record = (title) => {
+    if (db) {
+        const delete_transaction = db.transaction("links", "readwrite")
+
+        const objectStore = delete_transaction.objectStore("links")
+
+        return new Promise((resolve, reject) => {
+            delete_transaction.oncomplete = () => {
+                console.log("Delete Transactions Complete.")
+                resolve(true)
+            }
+    
+            delete_transaction.onerror = () => {
+                console.log("Error deleting records.")
+                resolve(false)
+            }
+    
+            objectStore.delete(title)
+        })
+    }
+}
+
+create_database()
+
---
+++ popup.js
@@ -0,0 +1,132 @@
+// Listen for messages received from backend (background.js). If message received, check message text and perform action.
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "get_all_success") {
+        if (request.payload) {
+            request.payload.forEach(link => {
+                addEntry(link.title, link.copyText)
+            })
+        }
+    } else if (request.message === "insert_success") {
+        if (request.payload) {
+            addEntry(request.payload.title, request.payload.copyText)
+
+            // Reset the inputs 
+            titleInput.value = ""
+            copyTextInput.value = ""
+        }
+    } else if (request.message === "delete_success") {
+        if (request.payload) {
+            // Remove linkGroup from DOM
+            recordToDelete.remove()
+        }
+    }
+})
+
+// Send message to backend to retrieve all records from database
+chrome.runtime.sendMessage({
+    message: "get_all",
+})
+
+// Select elements on the page for DOM manipulation
+const main = document.getElementById("main")
+const addForm = document.getElementById("add-form")
+const titleInput = document.getElementById("title-input")
+const copyTextInput = document.getElementById("copy-text-input")
+
+// Listen for form submit
+addForm.addEventListener("submit", (evt) => {
+    evt.preventDefault()
+
+    const formData = new FormData(addForm)
+
+    // Send message and data to backend to insert new record into database 
+    chrome.runtime.sendMessage({
+        message: "insert",
+        payload: {
+            "title": formData.get("title"),
+            "copyText": formData.get("copy-text")
+        }
+    })
+})
+
+// Copy text on click
+const addCopyEventListener = () => {
+    const copyBtns = document.getElementsByClassName("copy-btn");
+    
+    for (let btn of copyBtns) {
+        // Select text to be copied
+        let copyText = btn.parentElement.previousElementSibling.innerText
+        btn.addEventListener("click", () => {
+            // Copy text to clipboard
+            navigator.clipboard.writeText(copyText)
+        })
+    }
+}
+
+// Global variable representing DOM element to be removed. The variable can be set when the delete button is clicked. Once confirmation is received that the record has been deleted from the database, the element can be removed from the DOM, from inside of the chrome.runtime.onMessage.addListener
+let recordToDelete
+
+// Delete record on click
+const addDeleteEventListener = () => {
+    const deleteBtns = document.getElementsByClassName("delete-btn");
+
+    for (let btn of deleteBtns) {
+        btn.addEventListener("click", () => {
+            // Set variable so that it can be removed inside of chrome.runtime.onMessage.addListener 
+            recordToDelete = btn.parentElement.parentElement
+
+            // Get title text
+            let titleText = btn.parentElement.previousElementSibling.previousElementSibling.innerText
+
+            // Save title without ": " 
+            title = titleText.slice(0, titleText.length - 1)
+            
+            // Send message to backened to trigger delete action and payload with title used to look up the record in the database
+            chrome.runtime.sendMessage({
+                message: "delete",
+                payload: title
+            })
+        })
+    }
+}
+
+// Generate HTML for new record
+const addEntry = (titleText, textToCopy) => {
+    const inputGroup = document.createElement("div")
+    const title = document.createElement("p")
+    const copyText = document.createElement("p")
+    const btnGroup = document.createElement("div")
+    const deleteBtn = document.createElement("button")
+    const copyBtn = document.createElement("button")
+
+    title.innerText = titleText + ": "
+    copyText.innerText = textToCopy
+    deleteBtn.innerText = "Delete"
+    copyBtn.innerText = "Copy"
+
+    inputGroup.classList.add("input-group")
+    title.classList.add("title")
+    copyText.classList.add("copy-text")
+    btnGroup.classList.add("btn-group")
+    copyBtn.classList.add("copy-btn")
+    deleteBtn.classList.add("delete-btn")
+
+    inputGroup.appendChild(title)
+    inputGroup.appendChild(copyText)
+    btnGroup.appendChild(deleteBtn)
+    btnGroup.appendChild(copyBtn)
+    inputGroup.appendChild(btnGroup)
+
+    main.appendChild(inputGroup)
+
+    addCopyEventListener()
+    addDeleteEventListener()
+}
+
+
+
+
+
+
+
+
